23e846c2b01ceae9eba32ce091ffc25403cbcd4b
unknown <fazaldkhan315@gmail.com>
2023-05-13 01:10:22 +0500

    fouth time

--- imagee.py
+++ imagee.py
@@ -1,42 +1,58 @@
+import multiprocessing
+import datetime
 import numpy as np
 import cv2
-import multiprocessing
 import schedule
-import datetime
 
 
-def get_images(current_time):
-        width, height, num_images=800, 800, 4
-        
-        blue_images = []
+only_store = True
 
-        for i in range(num_images):
-            image = np.zeros((height, width, 4), dtype=np.uint8)
-            image[:,:,i] = 255
-            #file path
-            filename = f"C:\\Users\\AI-Team\\Desktop\\num\\Saveblue\\{current_time}.png"
-            cv2.imwrite(filename, image)
-            blue_images.append(image)
-        return blue_images
+def get_blue(now_time):
+    width, height, count_img = 800, 800, 2
+    blueimg = []
+    for i in range(count_img):
+        img = np.zeros((height, width, 3), dtype=np.uint8)
+        img[:,:,i] = 255
+        filename = f"C:\\Users\\AI-Team\\Desktop\\num\\Saveblue\\{now_time}_{i}.png"
+        cv2.imwrite(filename, img)
+        blueimg.append(img)
+    return blueimg
+
+def store_On():
+    global only_store
+    only_store = True
+
+def store_Off():
+    global only_store
+    only_store = False
+
+def capturing(param):
+    if param:
+        now_time = datetime.datetime.now().strftime("%H_%M_%S")
+        get_blue(now_time)
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    print("start programming "+ datetime.datetime.now().strftime("%H:%M:%S"))
+    
+    schedule.every().day.at("21:41").do(store_On)
+    schedule.every().day.at("23:00").do(store_Off)
+
+    
+    def start():
+            chan1 = multiprocessing.Process(target=capturing, args=(only_store,))
+            chan2 = multiprocessing.Process(target=capturing, args=(only_store,))
+
+            chan1.start()
+            chan2.start()
+
+            chan1.join()
+            chan2.join()
+    
+    
+    schedule.every(1).second.do(start)
+    while True:
+        schedule.run_pending()
 
 
-current_time = datetime.datetime.now().strftime("%H_%M_%S")
-#print(current_time)
-#get_blue_images(current_time)
-
-def start():
-      chan1 = multiprocessing.Process()
-      chan2 = multiprocessing.Process()
-
-      chan1.start()
-      chan2.start()
-
-      chan1.join()
-      chan2.join()
-
-
-      schedule.every(1).second.do(start)
-
-
-while True:
-      schedule.run_pending()
